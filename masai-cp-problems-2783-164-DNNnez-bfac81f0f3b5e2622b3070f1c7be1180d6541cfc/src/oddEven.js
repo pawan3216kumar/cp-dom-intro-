f4e4d11d5126eac38fc28d2ec0edef56b5aa9e1c
pawan3216kumar <dhananjay965114@gmail.com>
2024-02-09 22:25:23 +0530

    first commit

---
+++ masai-cp-problems-2783-164-DNNnez-bfac81f0f3b5e2622b3070f1c7be1180d6541cfc/src/oddEven.js
@@ -0,0 +1,22 @@
+function generateNumber() {
+  // generate a random integer(hint : Math.random)
+  console.log(getRandomInt(3));
+
+}
+
+function checkOddEven(num) {
+  // logic for even / odd
+}
+
+window.onload = function () {
+  // add event listeners to the button here
+};
+
+// donot change the following export statement
+
+if (typeof exports !== "undefined") {
+  module.exports = {
+    generateNumber,
+    checkOddEven,
+  };
+}
